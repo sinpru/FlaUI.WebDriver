81ad12059fa3ded280366535c1db77bce5854ef2
sinpru <contact@sinpru.com>
2025-11-13 11:08:03 +0700

    Fix CSS selector support for numeric IDs and escape sequences

--- src/FlaUI.WebDriver.UnitTests/Services/ConditionParserTests.cs
+++ src/FlaUI.WebDriver.UnitTests/Services/ConditionParserTests.cs
@@ -1,22 +1,79 @@
-﻿using FlaUI.WebDriver.Services;
+﻿using FlaUI.Core.Conditions;
+using FlaUI.UIA3;
+using FlaUI.WebDriver.Services;
 using NUnit.Framework;
 
 namespace FlaUI.WebDriver.UnitTests.Services
 {
     public class ConditionParserTests
     {
-        [TestCase("[name=\"2\"]")]
-        [TestCase("*[name=\"2\"]")]
-        [TestCase("*[name = \"2\"]")]
-        public void ParseCondition_ByCssAttributeName_ReturnsCondition(string selector)
+        private ConditionParser _conditionParser;
+        private ConditionFactory _conditionFactory;
+
+        [SetUp]
+        public void Setup()
         {
-            var parser = new ConditionParser();
-            var uia3 = new UIA3.UIA3Automation();
+            _conditionParser = new ConditionParser();
+            var automation = new UIA3Automation();
+            _conditionFactory = automation.ConditionFactory;
+        }
 
-            var result = parser.ParseCondition(uia3.ConditionFactory, "css selector", selector);
+        [Test]
+        public void ParseCondition_CssSelectorWithNumericIdUsingUnicodeEscape_ReturnsAutomationIdCondition()
+        {
+            var cssSelector = @"#\34 b090d48-e3a5-4eb4-bd37-4bd62dfa6e5b";
 
-            Assert.That(result.Property, Is.EqualTo(uia3.PropertyLibrary.Element.Name));
-            Assert.That(result.Value, Is.EqualTo("2"));
+            var condition = _conditionParser.ParseCondition(_conditionFactory, "css selector", cssSelector);
+
+            Assert.That(condition.Value, Is.EqualTo("4b090d48-e3a5-4eb4-bd37-4bd62dfa6e5b"));
+        }
+
+        [Test]
+        public void ParseCondition_CssSelectorWithSimpleNumericId_ReturnsAutomationIdCondition()
+        {
+            var cssSelector = @"#\31 ";
+
+            var condition = _conditionParser.ParseCondition(_conditionFactory, "css selector", cssSelector);
+
+            Assert.That(condition.Value, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void ParseCondition_CssSelectorWithEscapedSpecialChars_ReturnsNameCondition()
+        {
+            var cssSelector = @"*[name=""ListBox\ Item\ \#1""]";
+
+            var condition = _conditionParser.ParseCondition(_conditionFactory, "css selector", cssSelector);
+
+            Assert.That(condition.Value, Is.EqualTo("ListBox Item #1"));
+        }
+
+        [Test]
+        public void ParseCondition_CssSelectorCompoundSelector_ThrowsUnsupportedOperation()
+        {
+            var cssSelector = "#foo.bar";
+
+            Assert.Throws<WebDriverResponseException>(() =>
+                _conditionParser.ParseCondition(_conditionFactory, "css selector", cssSelector));
+        }
+
+        [Test]
+        public void ParseCondition_PlainIdStrategy_ReturnsAutomationIdCondition()
+        {
+            var id = "TextBox";
+
+            var condition = _conditionParser.ParseCondition(_conditionFactory, "id", id);
+
+            Assert.That(condition.Value, Is.EqualTo("TextBox"));
+        }
+
+        [Test]
+        public void ParseCondition_CssSelectorCompoundAttributeSelector_ThrowsUnsupportedOperation()
+        {
+            var cssSelector = "[name=\"test\"][class=\"test2\"]";
+
+            Assert.Throws<WebDriverResponseException>(() =>
+                _conditionParser.ParseCondition(_conditionFactory, "css selector", cssSelector));
         }
     }
 }
--- src/FlaUI.WebDriver/Services/ConditionParser.cs
+++ src/FlaUI.WebDriver/Services/ConditionParser.cs
@@ -9,41 +9,44 @@ namespace FlaUI.WebDriver.Services
         /// <summary>
         /// Based on https://www.w3.org/TR/CSS21/grammar.html (see also https://www.w3.org/TR/CSS22/grammar.html)
         /// Limitations: 
-        /// - Unicode escape characters are not supported.
         /// - Multiple selectors are not supported.
         /// </summary>
-        private static Regex SimpleCssIdSelectorRegex = new Regex(@"^#(?<name>(?<nmchar>[_a-z0-9-]|[\240-\377]|(?<escape>\\[^\r\n\f0-9a-f]))+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex SimpleCssIdSelectorRegex = new Regex(@"^#(?<name>(?<nmchar>[_a-z0-9-]|[\240-\377]|(?<escape>\\[^\r\n\f0-9a-f])|(?<unicode>\\[0-9a-fA-F]{1,6}\s?))+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         /// <summary>
         /// Based on https://www.w3.org/TR/CSS21/grammar.html (see also https://www.w3.org/TR/CSS22/grammar.html)
         /// Limitations: 
-        /// - Unicode escape characters are not supported.
         /// - Multiple selectors are not supported.
         /// </summary>
-        private static Regex SimpleCssClassSelectorRegex = new Regex(@"^\.(?<ident>-?(?<nmstart>[_a-z]|[\240-\377])(?<nmchar>[_a-z0-9-]|[\240-\377]|(?<escape>\\[^\r\n\f0-9a-f]))*)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex SimpleCssClassSelectorRegex = new Regex(@"^\.(?<ident>-?(?<nmstart>[_a-z]|[\240-\377])(?<nmchar>[_a-z0-9-]|[\240-\377]|(?<escape>\\[^\r\n\f0-9a-f])|(?<unicode>\\[0-9a-fA-F]{1,6}\s?))*)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         /// <summary>
         /// Based on https://www.w3.org/TR/CSS21/grammar.html (see also https://www.w3.org/TR/CSS22/grammar.html)
         /// Limitations: 
-        /// - Unicode escape characters or escape characters in the attribute name are not supported.
+        /// - Escape characters in the attribute name are not supported.
         /// - Multiple selectors are not supported.
         /// - Attribute presence selector (e.g. `[name]`) not supported.
         /// - Attribute equals attribute (e.g. `[name=value]`) not supported.
         /// - ~= or |= not supported.
         /// </summary>
-        private static Regex SimpleCssAttributeSelectorRegex = new Regex(@"^\*?\[\s*(?<ident>-?(?<nmstart>[_a-z]|[\240-\377])(?<nmchar>[_a-z0-9-]|[\240-\377])*)\s*=\s*(?<string>(?<string1>""(?<string1value>([^\n\r\f\\""]|(?<escape>\\[^\r\n\f0-9a-f]))*)"")|(?<string2>'(?<string2value>([^\n\r\f\\']|(?<escape>\\[^\r\n\f0-9a-f]))*)'))\s*\]$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex SimpleCssAttributeSelectorRegex = new Regex(@"^\*?\[\s*(?<ident>-?(?<nmstart>[_a-z]|[\240-\377])(?<nmchar>[_a-z0-9-]|[\240-\377])*)\s*=\s*(?<string>(?<string1>""(?<string1value>([^\n\r\f\\""]|(?<escape>\\[^\r\n\f0-9a-f])|(?<unicode>\\[0-9a-fA-F]{1,6}\s?))*)"")|(?<string2>'(?<string2value>([^\n\r\f\\']|(?<escape>\\[^\r\n\f0-9a-f])|(?<unicode>\\[0-9a-fA-F]{1,6}\s?))*)'))\s*\]$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         /// <summary>
         /// Based on https://www.w3.org/TR/CSS21/grammar.html (see also https://www.w3.org/TR/CSS22/grammar.html)
-        /// Limitations: 
-        /// - Unicode escape characters are not supported.
+        /// Matches simple escape characters (e.g., \#)
         /// </summary>
         private static Regex SimpleCssEscapeCharacterRegex = new Regex(@"\\[^\r\n\f0-9a-f]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Matches CSS unicode escape sequences (e.g., \34 or \000034 followed by optional space)
+        /// </summary>
+        private static Regex CssUnicodeEscapeRegex = new Regex(@"\\([0-9a-fA-F]{1,6})\s?", RegexOptions.Compiled);
 
         public PropertyCondition ParseCondition(ConditionFactory conditionFactory, string @using, string value)
         {
             switch (@using)
             {
+                case "id":
                 case "accessibility id":
                     return conditionFactory.ByAutomationId(value);
                 case "name":
@@ -86,8 +89,16 @@ namespace FlaUI.WebDriver.Services
 
         private static string ReplaceCssEscapedCharacters(string value)
         {
-            return SimpleCssEscapeCharacterRegex.Replace(value, match => match.Value.Substring(1));
-        }
+            var result = CssUnicodeEscapeRegex.Replace(value, m => 
+            {
+                var hexValue = m.Groups[1].Value;
+                var decodedChar = ((char)Convert.ToInt32(hexValue, 16)).ToString();
+                return decodedChar;
+            });
 
+            result = SimpleCssEscapeCharacterRegex.Replace(result, match => match.Value.Substring(1));
+            
+            return result;
+        }
     }
 }
